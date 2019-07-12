eafad09bcf890184609ae484d599fd5fe66e5d0c
Everton Fraga <ev@ethereum.org>
2019-07-12 15:16:57 -0400

    fix: implement os filtering

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-const platform = process.platform === 'win32' ? 'windows' : process.platform;
+let osFilter
+switch (process.platform) {
+    case 'win32': 
+        osFilter = 'windows'
+        break
+    case 'darwin': 
+        osFilter = 'darwin-amd64'
+        break
+    default:
+        osFilter = process.platfrorm;
+}
 
 module.exports = {
   type: 'storage',
   order: 3,
-  displayName: 'IPFS',
-  name: 'ipfs',
+  displayName: 'IPFS-local',
+  name: 'ipfslocal',
   repository: 'https://github.com/ipfs/go-ipfs',
   binaryName: process.platform === 'win32' ? 'ipfs.exe' : 'ipfs',
   beforeStart: {
     execute: [['init']]
+  },
+  filter: {
+    name: {
+        includes: [osFilter]
+    }
   },
   settings: [
     {
